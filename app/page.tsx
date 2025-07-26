00277ca74bd600c4e27c32957994895a7c649b92
abhinavsharmax <19abhinav19@gmail.com>
2025-07-26 12:08:20 +0530

    week 16

--- app/page.tsx
+++ app/page.tsx
@@ -98,7 +98,7 @@ const DynamicBarMenu = dynamic(
 const upcomingEvents: Event[] = [
   {
     name: "Weekend Brunch",
-    image: "/img/events/week15.jpg",
+    image: "/img/events/week16.jpg",
   },
 ];
 
