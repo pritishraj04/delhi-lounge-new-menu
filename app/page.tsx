b2c1ee6553c65c7aed37cf869f6eaa3b0b672d6e
abhinavsharmax <19abhinav19@gmail.com>
2025-10-05 17:47:10 +0530

    DLB

--- app/page.tsx
+++ app/page.tsx
@@ -98,7 +98,7 @@ const DynamicBarMenu = dynamic(
 // Sample upcoming events data
 const upcomingEvents: Event[] = [
   {
-    name: "Weekend Brunch",
+    name: "Diwali Lunch Buffet",
     image: "/img/events/diwalispecial.jpeg",
   },
 ];
